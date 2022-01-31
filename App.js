6114284e4c5aad44a2a0a781a044992ad4f7b529
juditlehoczki <judit.lehoczki@bankifi.com>
2022-01-31 12:53:33 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import { Provider } from "react-redux";
 
-export default function App() {
+import store from "./src/store/store";
+import RootNavigator from "./src/navigation/RootNavigator";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <RootNavigator />
+      </NativeBaseProvider>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
